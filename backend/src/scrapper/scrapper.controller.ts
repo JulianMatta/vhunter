b7199d85456f894f1bafd0b3f60e2acb71a99ab6
Julian Andres Rotili  Matta <jarotili@teco.com.ar>
2022-07-25 13:46:35 +0000

    Update scrapper.controller.ts

--- backend/src/scrapper/scrapper.controller.ts
+++ backend/src/scrapper/scrapper.controller.ts
@@ -1,12 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Query } from '@nestjs/common';
 import { ScrapperService } from "./scrapper.service";
 
 @Controller('scrapper')
 export class ScrapperController {
   constructor(private scrapperService: ScrapperService) { }
   @Get('/')
-  selectorComponentType(@Body('versionURL') versionURL: string, @Body('componentType') componentType: string) {
+  selectorComponentType(@Query('versionURL') versionURL: string, @Query('componentType') componentType: string) {
     return this.scrapperService.selectorComponentType(versionURL, componentType);
   }
 }
